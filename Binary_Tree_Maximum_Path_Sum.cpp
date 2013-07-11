1ba56a6b9c33a13746a160012eeb6eb5b37ae349
lcc <lcc@lccf>
2013-07-11 09:07:09 +0800

    max sum path should include at least one node, not return 0!

---
+++ Binary_Tree_Maximum_Path_Sum.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int maxPathSum(TreeNode *root) {
+        int res = -999999999;
+         dfs(root, res);
+         return res;
+    }
+    int dfs(TreeNode* root, int &res){
+        if (root == NULL){
+            return 0;
+        }
+        int a = root->val;
+        int b = dfs(root->left, res);
+        int c = dfs(root->right, res);
+        res = max(a, res);
+        res = max(a+b, res);
+        res = max(a+c, res);
+        res = max(a+b+c, res);
+        return a + max(0, max(b,c));
+    }
+};
