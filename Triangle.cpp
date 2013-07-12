355de01537292b15810d826b409f97739596a14d
lcc <lcc@lccf>
2013-07-12 15:46:13 +0800

    ..

---
+++ Triangle.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int minimumTotal(vector<vector<int> > &triangle) {
+        int n = triangle.size();
+        vector<int> a[2];
+        for (int i = 0; i<n; i++){
+            a[0].push_back(0);
+            a[1].push_back(0);
+        }
+        int t = 0;
+        a[t][0] = triangle[0][0];
+        for (int i = 1; i<n; i++){
+            a[t^1][0] = a[t][0] + triangle[i][0];
+            for (int j = 1; j<i; j++){
+                a[t^1][j] = min(a[t][j] , a[t][j-1]) + triangle[i][j];
+            }
+            a[t^1][i] = a[t][i-1] + triangle[i][i];
+            t^=1;
+        }
+        int res = 999999999;
+        for (int i = 0; i<n; i++){
+            res = min(res, a[t][i]);
+        }
+        return res;
+    }
+};
