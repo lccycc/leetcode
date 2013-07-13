9ae9598ff115e2376719eb8475df50bd8286b1b1
lcc <lcc@lccf>
2013-07-13 09:45:08 +0800

    take care of n = 0

---
+++ Pascals_Triangle.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<int> > generate(int numRows) {
+        vector<vector<int> > res;
+        vector<int> tp;
+ 
+        for (int i = 0; i<numRows; i++){
+            tp.push_back(1);
+            for (int j = 1; j<i; j++){
+                tp[j] = res[i-1][j-1]+res[i-1][j];
+            }
+            res.push_back(tp);
+        }
+        return res;
+    }
+};
