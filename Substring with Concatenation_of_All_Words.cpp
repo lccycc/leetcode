3089ff55e9f56fff103dfec3f99b89714239ce1b
lccycc <lccycc123@gmail.com>
2013-09-13 16:58:24 +0800

    Update Substring with Concatenation_of_All_Words.cpp
    O(   n*L[0].size()*log(L.size())   )

--- Substring with Concatenation_of_All_Words.cpp
+++ Substring with Concatenation_of_All_Words.cpp
@@ -1,6 +1,15 @@
 class Solution {
 public:
     vector<string> simpL;
+    vector<int> bkcnt;
+    int nozero;
+    void valid(int kd, int sgn){
+        if (kd != -1){
+            if (bkcnt[kd] == 0) nozero++;
+            bkcnt[kd] += sgn;
+            if (bkcnt[kd] == 0) nozero--;
+        }
+    }
     int search(string s){
         int l = 0, r = simpL.size();
         while (l+1<r){
@@ -25,10 +34,11 @@ public:
             }
             return res;
         }
+        int Lsize = L.size();
         sort(L.begin(), L.end());
         vector<int> cnt;
         simpL.clear();
-        for (int i = 0; i<L.size(); i++){
+        for (int i = 0; i<Lsize; i++){
             int counter = 1;
             while (i+1<L.size() && L[i].compare(L[i+1]) == 0){
                 i++;
@@ -38,21 +48,29 @@ public:
             simpL.push_back(L[i]);
         }
         int comLen = L[0].size();
-        for (int i = 0; i+L.size()*comLen<=S.size(); i++){
-            vector<int> bkcnt = cnt;
-            bool err = false;
-            for (int j = 0; j<L.size(); j++){
-                int pos = i + j * comLen;
-                string sub = S.substr(pos, comLen);
-                int fd = search(sub);
-                if (fd == -1 || --bkcnt[fd]<0) {
-                    err = true; break;
+        
+        for (int start = 0; start<comLen && start + comLen*Lsize <= S.size(); start++){
+            vector<int> arr;
+            for (int i = start; i<S.size(); i+=comLen){
+                arr.push_back(search(S.substr(i, comLen)));
+            }
+            bkcnt = cnt;
+            nozero = bkcnt.size();
+            for (int j = 0; j<Lsize; j++){
+                valid(arr[j], -1);
+            }
+            if (nozero == 0){
+                res.push_back(start);
+            }
+            for (int i = 0; i+Lsize<arr.size(); i++){
+                valid(arr[i], 1);
+                valid(arr[i+Lsize], -1);
+                if (nozero == 0){
+                    res.push_back(start + comLen*(i+1));
                 }
             }
-            if (!err){
-                res.push_back(i);
-            }
         }
+        sort(res.begin(), res.end());
         return res;
     }
 };
