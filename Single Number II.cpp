89816059e9c0f3b0ed57ca02905ea1a21c98bcf1
lccycc <lccycc123@gmail.com>
2013-10-09 18:43:08 +0800

    Update Single Number II.cpp

--- Single Number II.cpp
+++ Single Number II.cpp
@@ -2,29 +2,12 @@ typedef unsigned long long LL;
 class Solution {
 public:
     int singleNumber(int A[], int n) {
-    	LL mask1, mask2;
-    	for (int i = 0; i<32; i++){
-    		mask1 = ((mask1<<2)|1);
-    	}
-    	mask2 = (mask1<<1);
-    	LL a = 0, b = 0, c = 0;
+    	int a = 0, b = 0;
     	for (int i = 0; i<n; i++){
-    		LL na = (unsigned int)A[i];
-    		LL od = (na & mask1);
-    		LL ev = (na & mask2);
-    		na = ((ev<<31) | od);
-    
-    		c = c + na;
-    
-    		a = (c & mask1);
-    		b = (c & mask2);
-    
-    		LL z = (a^(b>>1));
-    		LL x = (a&z);
-    		LL y = (b&(z<<1));
-    
-    		c = (x | y);
+    		int nb = ~(a^b);
+    		a = (A[i]&b)|((~A[i])&a);
+    		b = (A[i]&nb)|((~A[i])&b);
     	}
-    	return (int) (c|(c>>31));
+    	return b;
     }
 };
