fc449153558ecc8fbece3f2452f629cece00b848
lccycc <lccycc123@gmail.com>
2013-10-15 15:47:39 +0800

    Update Binary Tree Inorder Traversal.cpp

--- Binary Tree Inorder Traversal.cpp
+++ Binary Tree Inorder Traversal.cpp
@@ -1,22 +1,38 @@
-class Solution {
+class Inorder{
+    stack<TreeNode *> s;
+    TreeNode *p;   
 public:
-    vector<int> inorderTraversal(TreeNode *root) {
-        vector<int> res;
-        if (root == NULL) return res;
-        stack<TreeNode *> s;
-        TreeNode *p =root;
-        while (true){
-            if (p){
+    Inorder(TreeNode* root){
+        p = root;
+        while (!s.empty()) s.pop();
+    }
+    TreeNode* next(){
+        TreeNode* res = NULL;
+        while (!res){
+            if (p == NULL){
+                if (s.empty()) break;
+                res = p = s.top();
+                s.pop();
+                p = p->right;
+            }else{
                 s.push(p);
                 p = p->left;
-            }else{
-                if (!s.size()) break;
-                p = s.top();
-                s.pop();
-                res.push_back(p->val);
-                p = p->right;
             }
         }
         return res;
+    }
+};
+
+
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode *root) {
+		vector<int> res;
+	    Inorder o(root);
+	    TreeNode *p;
+	    while ((p = o.next()) != NULL){
+	        res.push_back(p->val);
+	    }
+		return res;
     }
 };
