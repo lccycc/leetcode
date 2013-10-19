ad1f6601293ae33a05dfd9f3604d1e6793831907
lccycc <lccycc123@gmail.com>
2013-10-19 10:18:10 +0800

    Create Permutations.cpp

---
+++ Permutations.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int> > permute(vector<int> &num) {
+        vector<vector<int> > res;
+        sort(num.begin(), num.end());
+        int n= num.size();
+        res.push_back(num);
+        if (n < 2) return res;
+        while (true){
+            int i = n-2;
+            while (i>=0 && num[i] >= num[i+1]) i--;
+            
+            if (i>=0){
+                int j= n-1;
+                while (num[j]<=num[i]){
+                    j--;
+                }
+                swap(num[i], num[j]);
+                for (int l = i+1, r = n-1; l<r; l++, r--)
+                    swap(num[l], num[r]);
+                res.push_back(num);
+            }else
+                break;
+        }
+        return res;
+    }
+};
