49ec0ca71229ae2713aeb4e03506d5fe979ee816
lccycc <lccycc123@gmail.com>
2013-10-19 10:38:58 +0800

    Create Remove Duplicates from Sorted List.cpp

---
+++ Remove Duplicates from Sorted List.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    ListNode *deleteDuplicates(ListNode *head) {
+        if (head == NULL) return NULL;
+		for (ListNode *p = head; p && p->next != NULL; p = p->next){
+			while (p->next != NULL && p->next->val == p->val){
+				p->next = p->next->next;
+			}
+		}
+		return head;        
+    }
+};
