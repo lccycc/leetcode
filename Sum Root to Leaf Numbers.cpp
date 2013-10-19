74a6ce8a4f91231be434b48edcac4407a9a5922e
lccycc <lccycc123@gmail.com>
2013-10-19 11:11:34 +0800

    Create  Sum Root to Leaf Numbers.cpp

---
+++ Sum Root to Leaf Numbers.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+	int tot;
+	void dfs(TreeNode *root, int s){
+		if (root == NULL) return ;
+		s = s*10 + root->val;
+		if (root->left == NULL && root->right == NULL){
+			tot += s;
+		}else{
+			dfs(root->left, s);
+			dfs(root->right, s);
+		}
+	}
+    int sumNumbers(TreeNode *root) {
+        tot = 0;
+		dfs(root, 0);
+		return tot;
+    }
+};
+
+
