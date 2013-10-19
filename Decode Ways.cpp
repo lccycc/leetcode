cf3f310f755f9309c8628ebe3924fc01f396295b
lccycc <lccycc123@gmail.com>
2013-10-19 10:48:23 +0800

    Create Decode Ways.cpp

---
+++ Decode Ways.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int numDecodings(string s) {
+        int f = 0, g = 1;
+        if (s.size() == 0) return 0;
+		for (int i = 0; i<s.size(); i++){
+			int c = 0;
+			if (s[i] != '0'){
+				c += g;
+			}
+			if (i && s[i-1]!='0' && (s[i-1]-'0')*10 + s[i]-'0' <=26){
+				c += f;
+			}
+			f = g; g = c;
+		}
+		return g;
+    }
+};
+
+
