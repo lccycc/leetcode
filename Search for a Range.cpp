fd2cd75030d548d2877fa1f37eb2fcf59df7ebd4
lccycc <lccycc123@gmail.com>
2013-10-19 10:11:42 +0800

    Create Search for a Range.cpp

---
+++ Search for a Range.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+	int lowerbound(int A[], int l, int r, int target){
+	    //find minimal l that A[l] >= target
+		while (l+1<r){
+			int mid = (l+r)/2;
+			if (A[mid] >= target){
+				r = mid;
+			}else{
+				l = mid;
+			}
+	    }
+	    if (A[l] < target) return l+1;
+	    return l;
+	}
+	int upperbound(int A[], int l, int r, int target){
+	    //find maximal l that A[l] <= target
+		while (l+1<r){
+			int mid = (l+r)/2;
+			if (A[mid] <= target){
+				l = mid;
+			}else{
+				r = mid;
+			}
+		}
+		if (A[l] > target) return l-1;
+		return l;
+}
+    vector<int> searchRange(int A[], int n, int target) {
+		vector<int> res;
+		res.push_back(-1);
+		res.push_back(-1);
+        int l = lowerbound(A, 0, n, target);
+		if (l >= n || A[l] != target){
+			return res;
+		}
+		int r = upperbound(A, 0, n, target);
+		res[0] = l;
+		res[1] = r;
+		return res;
+	}	
+};
