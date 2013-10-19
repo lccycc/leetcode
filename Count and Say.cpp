b5be62ec33a836e3e70d9686dc63df46c22249b7
lccycc <lccycc123@gmail.com>
2013-10-19 10:13:52 +0800

    Create Count and Say.cpp

---
+++ Count and Say.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string countAndSay(int n) {
+        string res = "1";
+        if (n == 0) return "";
+        for (int t = 2; t<=n; t++){
+            string tp = "";
+            for (int i = 0; i<res.size();){
+                int j = i+1;
+                while (j<res.size() && res[i] == res[j]) j++;
+                
+                char tmp[50];
+                sprintf(tmp, "%d%c", j-i, res[i]);
+                tp = tp + tmp;
+                i = j;
+            }
+            res = tp;
+        }
+            return res;
+    }
+};
