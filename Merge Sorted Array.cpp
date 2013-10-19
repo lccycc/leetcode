f762590ae8609684e5649ad90705a282eb9dec9b
lccycc <lccycc123@gmail.com>
2013-10-19 10:47:20 +0800

    Create Merge Sorted Array.cpp

---
+++ Merge Sorted Array.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void merge(int A[], int m, int B[], int n) {
+        int i = m-1, j = n-1, k = m+n-1;
+		while (k>=0){
+			if (i>=0 && (j<0 || A[i]>B[j])){
+				A[k--] = A[i--];
+			}else{
+				A[k--] = B[j--];
+			}
+		}      
+    }
+};
+
