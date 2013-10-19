965489117db12dec5bbe659cad32209cbf3a2950
lccycc <lccycc123@gmail.com>
2013-10-19 10:25:47 +0800

    Create Rotate List.cpp

---
+++ Rotate List.cpp
@@ -0,0 +1,28 @@
+
+class Solution {
+public:
+    ListNode *rotateRight(ListNode *head, int k) {
+        if (head == NULL || k == 0) return head;
+        ListNode newhead(0), *p = &newhead;
+        newhead.next = head;
+        int i = 0;
+        while (i<k){
+            p = p->next;
+            i++;
+            if (p->next == NULL) break;
+        }
+        if (p->next == NULL){
+            return rotateRight(head, k%i);
+        }
+        ListNode *q = &newhead;
+        while (p->next != NULL){
+            p = p->next;
+            q = q->next;
+        }
+        p->next = head;
+        head = q->next;
+        q->next = NULL;
+        return head;        
+    }
+};
+
