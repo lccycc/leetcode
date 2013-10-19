2ad864a2012374d125e3defd42157564d0129351
lccycc <lccycc123@gmail.com>
2013-10-19 10:22:27 +0800

    Create Jump Game.cpp

---
+++ Jump Game.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool canJump(int A[], int n) {
+        if (n<=1) return true;
+        int r = 0;
+        for (int i = 0; i<=r; i++){
+            r = max(r, A[i]+i);
+            if (r>=n-1) return true;
+        }
+        return false;
+        
+    }
+};
+
