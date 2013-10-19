02a5a7e0fd1ba0461c81db6bb8cd55d41d256bea
lccycc <lccycc123@gmail.com>
2013-10-19 11:06:45 +0800

    Create Path Sum II.cpp

---
+++ Path Sum II.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+	vector<int> s;
+	vector<vector<int> > res;
+	void dfs(TreeNode *root, int sum){
+		if (root == NULL) return ;
+		s.push_back(root->val);
+		sum -= root->val;
+		if (root->left == NULL && root->right == NULL){
+			if (sum == 0){
+				res.push_back(s);
+			}
+		}else{
+        	dfs(root->left, sum);
+        	dfs(root->right, sum);
+        }
+	    s.resize(s.size()-1);
+    }
+    vector<vector<int> > pathSum(TreeNode *root, int sum) {
+        s.clear();
+		res.clear();
+		if (root ==NULL) return res;
+		dfs(root, sum);
+		return res;
+    }
+};
