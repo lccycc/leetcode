400ef4815703a8f6b8ec85f50243e51c89d68f58
lccycc <lccycc123@gmail.com>
2013-10-19 10:24:06 +0800

    Create Insert Interval.cpp

---
+++ Insert Interval.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for an interval.
+ * struct Interval {
+ *     int start;
+ *     int end;
+ *     Interval() : start(0), end(0) {}
+ *     Interval(int s, int e) : start(s), end(e) {}
+ * };
+ */
+class Solution {
+public:
+    vector<Interval> insert(vector<Interval> &intervals, Interval newInterval) {
+        vector<Interval> res;
+        int n = intervals.size();
+        int i = 0;
+        for (; i<n && intervals[i].end < newInterval.start; i++){
+            res.push_back(intervals[i]);
+        }
+        for (; i<n && intervals[i].start <= newInterval.end; i++){
+            newInterval.start = min(newInterval.start, intervals[i].start);
+            newInterval.end = max(newInterval.end, intervals[i].end);
+        }
+        res.push_back(newInterval);
+        for (; i<n; i++){
+            res.push_back(intervals[i]);
+        }
+        return res;        
+    }
+};
+
