907d6a52659a718a0c6a5c87c32603ab802fd8c9
lccycc <lccycc123@gmail.com>
2013-10-19 11:12:16 +0800

    Update Surrounded_Regions.cpp

--- Surrounded_Regions.cpp
+++ Surrounded_Regions.cpp
@@ -1,61 +1,53 @@
 class Solution {
 public:
-    const int dx[4] = {-1,0,1,0};
-    const int dy[4] = {0,1,0,-1};
-    void solve(vector<vector<char>> &board) {
-        vector<vector<bool>> vis;
-        vector<pair<int, int>> q;
-
-        int n = board.size();
-        if (n == 0 ) return ;
-        int m = board[0].size();
-        if (m == 0) return ;
-        vector<bool> nb;
-        for (int j = 0; j<m; j++){
-            nb.push_back(false);
-        }
-        for (int i = 0; i<n; i++){
-            vis.push_back(nb);
-        }
-        for (int i = 0; i<n; i++){
-            for (int  j = 0; j<m; j++){
-                if (vis[i][j] == false && board[i][j] == 'O'){
-                    bool err = false;
-                    q.clear();
-                    q.push_back(make_pair(i,j));
-                    vis[i][j] = false;
-                    for (int t = 0; t<q.size(); t++){
-                        int x = q[t].first;
-                        int y = q[t].second;
-                        for (int r = 0; r<4; r++){
-                            int nx = x + dx[r];
-                            int ny = y + dy[r];
-                            if (nx>=0 && nx<n && ny>=0 && ny<m){
-                                if (board[nx][ny] == 'O'){
-                                    if (!vis[nx][ny]){
-                                        q.push_back(make_pair(nx, ny));
-                                        vis[nx][ny] = true;
-                                    }
-                                }else
-                                if (board[nx][ny]!='X'){
-                                    err = false;
-                                }
-                            }else{
-                                err = true;
-                            }
-                        }
-                    }
-                    if (!err){
-                        for (int t = 0; t < q.size(); t++){
-                            board[q[t].first][q[t].second] = 'X';
-                        }
-                    }
+    vector<vector<bool> > visit;
+    void bfs(vector<vector<char> > &board, int x, int y, int n, int m){
+        vector<pair<int, int> > h;
+        visit[x][y] = true;
+        h.push_back(make_pair(x, y));
+        const int f[4] = {-1, 0, 1, 0}, g[4] = {0, 1, 0, -1};
+        bool meetbd = false;
+        for (int i = 0; i<h.size(); i++){
+            x = h[i].first, y = h[i].second;
+            for (int j = 0; j<4; j++){
+                int u = x + f[j], v = y + g[j];
+                if (u < 0 || u >= n || v < 0 || v >= m){
+                    meetbd = true;
+                    continue;
                 }
+                if (board[u][v] == 'X' || visit[u][v]){
+                    continue;
+                }
+                h.push_back(make_pair(u, v));
+                visit[u][v] = true;
+            }
+        }
+        if (!meetbd){
+            for (int i = 0; i<h.size(); i++){
+                board[h[i].first][h[i].second] = 'X';
             }
         }
     }
+    void solve(vector<vector<char>> &board) {
+         int n = board.size();
+        if (n == 0) return ;
+        int m = board[0].size();
+        if (m == 0) return ;
+        visit.clear();
+        for (int i = 0; i<n; i++){
+            vector<bool> tmp;
+            for (int j = 0; j<m; j++){
+                tmp.push_back(0);
+            }
+            visit.push_back(tmp);
+        }
+        for (int i = 0; i<n; i++){
+            for (int j = 0; j<m; j++){
+                if (board[i][j] == 'O' && visit[i][j] == false){
+                    bfs(board, i, j, n, m);
+                }
+            }
+        }
+        visit.clear();
+    }
 };
-
-
-
-
