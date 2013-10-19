0f1ada795d046b87e8634475c89377bf812bf969
lccycc <lccycc123@gmail.com>
2013-10-19 11:15:56 +0800

    Create Gas Station.cpp

---
+++ Gas Station.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int canCompleteCircuit(vector<int> &gas, vector<int> &cost) {
+        int tot = 0, n = gas.size();
+		if (n == 0) return -1;
+		bool fd = false;
+		int mxid, mxtot;
+		for (int i = 0; i<gas.size(); i++){
+			tot = tot + gas[i] - cost[i];
+			if (!fd || mxtot > tot){
+				fd = true;
+				mxtot = tot;
+				mxid = i;
+			}
+		}
+		if (tot < 0) return -1;
+		return (mxid+1)%n;
+        
+    }
+};
+
+
