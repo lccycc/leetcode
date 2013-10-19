86c43148f0b011724994e662cc1d08a62e859f63
lccycc <lccycc123@gmail.com>
2013-10-19 10:47:48 +0800

    Create Gray Code.cpp

---
+++ Gray Code.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    vector<int> grayCode(int n) {
+        vector<int> res;
+		for (int i = 0; i<(1<<n); i++){
+			res.push_back(i^(i>>1));
+        }
+		return res;
+    }
+};
