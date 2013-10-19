343962d7184684af52aa417d1329688e3c651f19
lccycc <lccycc123@gmail.com>
2013-10-19 10:56:53 +0800

    Create Validate Binary Search Tree.cpp

---
+++ Validate Binary Search Tree.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+	bool err;
+	pair<int, int> dfs(TreeNode *t){
+		pair<int, int> l, r;
+		int lbound = t->val, rbound = t->val;
+		if (t->left){
+			l = dfs(t->left);
+			if (l.second >= t->val) err = true;
+			lbound = l.first;
+		}
+		if (t->right){
+			r = dfs(t->right);
+			if (r.first <= t->val) err = true;
+			rbound = r.second;
+		}
+		return make_pair(lbound, rbound);
+    }
+    bool isValidBST(TreeNode *root) {
+		if (root == NULL) return true;
+        err = false;
+        dfs(root);
+		return !err;
+    }
+};
