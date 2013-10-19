c636946c802f122da4171bd9b2c331704bae442d
lccycc <lccycc123@gmail.com>
2013-10-19 10:34:12 +0800

    Create Sort Colors.cpp

---
+++ Sort Colors.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void sortColors(int A[], int n) {
+		int i = 0, j = n-1;
+		for (int k = 0; k<=j; k++){
+		while (A[k] == 2 && k<j){
+			swap(A[k], A[j--]);
+		}
+		if (A[k] == 0){
+			swap(A[k], A[i++]);
+		}
+	}
+}
+};
+
+
