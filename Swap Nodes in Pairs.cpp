851cff449eea8462669855c10339d2bb0b7c7f89
lccycc <lccycc123@gmail.com>
2013-10-19 09:59:51 +0800

    Create Swap Nodes in Pairs.cpp

---
+++ Swap Nodes in Pairs.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    ListNode *swapPairs(ListNode *head) {
+        ListNode newhead(0), *p = &newhead;
+		newhead.next = head;
+		while (p->next && p->next->next){
+			ListNode *p1 = p->next, *p2 = p1->next, *p3 = p2->next;
+        	p1->next = p3;
+			p2->next = p1;
+			p->next = p2;
+			p = p1;
+		}
+		return newhead.next;
+    }
+};
