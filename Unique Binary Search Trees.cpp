44db47d8200d72bf93bcf189060985f67b15a048
lccycc <lccycc123@gmail.com>
2013-10-19 10:51:12 +0800

    Create Unique Binary Search Trees.cpp

---
+++ Unique Binary Search Trees.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int numTrees(int n) {
+        vector<int> a;
+		a.push_back(1);
+		for (int i = 1; i<=n; i++){
+			int tot = 0;
+			for (int j = 0; j<=i-1; j++){
+				tot += a[j]*a[i-1-j];
+			}
+			a.push_back(tot);
+		}
+        return a[n];
+    }
+};
+
+
