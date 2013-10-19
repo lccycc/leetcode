f909e944cec01849d318fb505bc4748f75ad23a3
lccycc <lccycc123@gmail.com>
2013-10-19 09:56:02 +0800

    Create Roman to Integer.cpp

---
+++ Roman to Integer.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        map<char, int> d;
+		d['I'] = 1;
+		d['V'] = 5;
+		d['X'] = 10;
+		d['L'] = 50;
+		d['C'] = 100;
+		d['D'] = 500;
+		d['M'] = 1000;
+		int res = 0;
+		for (int i = 0; i<s.size(); i++){
+        	if (i+1<s.size() && d[s[i]] < d[s[i+1]]){
+				res -= d[s[i]];
+			}else{
+				res += d[s[i]];
+			}
+		}
+		return res;
+    }
+};
