d24769dfccf0eb8363441ac4059573afc234b98d
lccycc <lccycc123@gmail.com>
2014-05-04 20:41:00 +0800

    Create Sort List.cpp

---
+++ Sort List.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *sortList(ListNode *head) {
+        if (head == NULL) return NULL;
+        int n = 0;
+        for (auto p = head; p; p = p->next){
+            n++;
+        }
+        int val = head->val;
+        n/=2;
+        for (auto p = head; n--; p = p->next){
+            val = p->val;
+        }
+        ListNode h1(0), h2(0), h3(0), *t1 = &h1, *t2 = &h2, *t3 = &h3;
+        for (auto p = head; p; p = p->next){
+            if (p->val == val){
+                t2 = t2->next = p;
+            }else if (p->val < val){
+                t1 = t1->next = p;
+            }else{
+                t3 = t3->next = p;
+            }
+        }
+        t1->next = t2->next = t3->next = NULL;
+        h1.next = sortList(h1.next);
+        for (t1 = &h1; t1->next; t1 = t1->next);
+        t1->next = h2.next;
+        t2->next = sortList(h3.next);
+        return h1.next;
+    }
+};
