0f94c19426638522aec19d11784b9164ed05f6af
lccycc <lccycc123@gmail.com>
2014-05-06 19:06:01 +0800

    Update Linked List Cycle II.cpp

--- Linked List Cycle II.cpp
+++ Linked List Cycle II.cpp
@@ -3,22 +3,11 @@ public:
     ListNode *detectCycle(ListNode *head) {
         auto p = head, q = head;
         if (!p) return NULL;
-        int n = 0;
         while (p){
             if (p->next == NULL || p->next->next == NULL) return NULL;
             p = p->next->next;
             q = q->next;
-            n++;
             if (q == p) break;
-        }
-        int l = 0;
-        while (true){
-            p = p->next;
-            l++;
-            if (p == q) break;
-        }
-        for (int i = 0; i<(l-n%l)%l; i++){
-            p = p->next;
         }
         while (head != p){
             head = head->next;
