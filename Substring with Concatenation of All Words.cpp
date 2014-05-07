63efef7f4d51225c97e4227960b410da10e64cff
lccycc <lccycc123@gmail.com>
2014-05-07 21:28:16 +0800

    Update Substring with Concatenation of All Words.cpp

--- Substring with Concatenation of All Words.cpp
+++ Substring with Concatenation of All Words.cpp
@@ -2,35 +2,28 @@ class Solution {
 public:
     vector<int> findSubstring(string S, vector<string> &L) {
         vector<int> res;
-        if (L.size() == 0){
-            for (int i = 0; i<S.size(); i++){
-                res.push_back(i);
-            }
+        if (L.size() == 0) return res;
+        int len = L[0].size();
+        if (len == 0){
+            for (int i = 0; i<S.size(); i++) res.push_back(i);
             return res;
         }
-        map<string, int> counter;
-        for (int i = 0; i<L.size(); i++){
-            counter[L[i]]++;
+        map<string, int> num;
+        for (string s:L){
+            num[s]++;
         }
-        int l = L[0].size();
-        for (int p = 0; p<l; p++){
-            map<string, int> bcnt = counter;
-            int cnt = counter.size();
-            for (int i = 0; ; i++){
-                int x = i*l+p;
-                if (x+l>S.size()) break;
-                if (--bcnt[S.substr(x, l)] == 0){
-                    cnt--;
+        for (int start = 0; start < len; start++){
+            auto a = num;
+            int cnt = a.size();
+            for (int i = 0; start+i*len+len<=S.size(); i++){
+                auto sub = S.substr(start+i*len, len);
+                if (--a[sub] == 0) cnt--;
+                if (cnt == 0){
+                    res.push_back(start+(i-L.size()+1)*len);
                 }
-                if (i>=L.size()-1){
-                    int y = x-L.size()*l+l;
-                    if (cnt == 0){
-                        res.push_back(y);
-                    }
-                    if (bcnt[S.substr(y, l)]++ == 0){
-                        cnt++;
-                    }
-                }
+                if (i<L.size()-1) continue;
+                sub = S.substr(start+(i-L.size()+1)*len, len);
+                if (a[sub]++ == 0) cnt++;
             }
         }
         return res;
