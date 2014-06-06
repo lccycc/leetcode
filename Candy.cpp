a7dc97fecc6afdf98718955aefd6594a27e91ff4
lccycc <lccycc123@gmail.com>
2014-06-06 23:05:11 +0800

    Update Candy.cpp

--- Candy.cpp
+++ Candy.cpp
@@ -1,37 +1,28 @@
 class Solution {
 public:
     int candy(vector<int> &ratings) {
-        vector<int> candy;
-        int n = ratings.size();
-        candy.resize(n);
-        candy[0] = 1;
-        stack<int> s;
-        s.push(ratings[0]);
-        for (int i = 1; i<n; i++){
-            candy[i] = 1;
-            if (ratings[i] >= s.top()){
-                if (ratings[i] > s.top()){
-                    candy[i] = max(candy[i], candy[i-1]+1);
-                }
-                int j = i-1;
-                s.pop();
-                while (s.size()){
-                    s.pop();
-                    j--;
-                    candy[j] = max(candy[j], candy[j+1]+1);
-                }
+        int sum = 0;
+        int last= 1;
+        for (int i = 0; i<ratings.size()-1;){
+            if (ratings[i] == ratings[i+1]){
+                sum += last;
+                last = 1;
+                i++;
+            }else if (ratings[i] < ratings[i+1]){
+                sum += last;
+                last++;
+                i++;
+            }else{
+                int j = i;
+                while (j<ratings.size()-1 && ratings[j] > ratings[j+1]) j++;
+                int m = j-i;
+                last = max(last, m+1);
+                sum += max(last, m+1) + m*(m+1)/2-1;
+                i = j;
+                last = 1;
             }
-            s.push(ratings[i]);
         }
-        s.pop();
-        int j = n-1;
-        while (s.size()){
-            s.pop();
-            j--;
-            candy[j] = max(candy[j], candy[j+1]+1);
-        }
-        int tot = 0;
-        for (int i = 0; i<n; i++) tot += candy[i];
-        return tot;
+        sum += last;
+        return sum;
     }
 };
