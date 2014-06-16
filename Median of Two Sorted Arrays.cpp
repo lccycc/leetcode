614ec24274af9d83a742b26adeabfe3984b8e03f
lccycc <lccycc123@gmail.com>
2014-06-16 16:08:28 +0800

    Update Median of Two Sorted Arrays.cpp

--- Median of Two Sorted Arrays.cpp
+++ Median of Two Sorted Arrays.cpp
@@ -1,3 +1,28 @@
+/*
+class Solution {
+public:
+    int findKth(int A[], int m, int B[], int n, int k){
+        if (m>n) return findKth(B, n, A, m, k);
+        if (m == 0) return B[k-1];
+        if (k == 1) return min(A[0], B[0]);
+        int la = min(k/2, m);
+        int lb = k-la;
+        if (A[la-1] >= B[lb-1]) return findKth(A, la, B+lb, n-lb, k-lb);
+        else return findKth(A+la, m-la, B, lb, k-la);
+    }
+    double findMedianSortedArrays(int A[], int m, int B[], int n) {
+        if ((m+n)%2){
+            return findKth(A, m, B,n, (m+n)/2+1);
+        }else{
+            int x = findKth(A, m, B,n, (m+n)/2);
+            int y = findKth(A, m, B,n, (m+n)/2+1);
+            return (x+(double)y)/2.0;
+        }
+    }
+};
+*/
+
+
 class Solution {
 public:
     double findMedianSortedArrays(int A[], int m, int B[], int n) {
