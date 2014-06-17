34870a2f86083f1ae8577d02661b4a84cd7f94c6
lccycc <lccycc123@gmail.com>
2014-06-17 10:30:43 +0800

    Update Binary Tree Level Order Traversal.cpp

--- Binary Tree Level Order Traversal.cpp
+++ Binary Tree Level Order Traversal.cpp
@@ -1,35 +1,20 @@
-/**
- * Definition for binary tree
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
+
 class Solution {
 public:
     vector<vector<int> > levelOrder(TreeNode *root) {
-        vector<TreeNode*> p[2];
+        vector<TreeNode *> f, g;
         vector<vector<int> > res;
-        if (root == NULL) return res;
-        int pnt = 0;
-        p[0].push_back(root);
-        while (p[pnt].size()){
+        if (!root) return res;
+        f.push_back(root);
+        while (f.size()){
             res.push_back(vector<int>());
-            for (int i = 0; i<p[pnt].size();i++){
-                res.back().push_back(p[pnt][i]->val);
+            g.clear();
+            for (auto x : f){
+                res.back().push_back(x->val);
+                if (x->left) g.push_back(x->left);
+                if (x->right) g.push_back(x->right);
             }
-            p[pnt^1].clear();
-            for (int i = 0; i<p[pnt].size();i++){
-                if (p[pnt][i]->left){
-                    p[pnt^1].push_back(p[pnt][i]->left);
-                }
-                if (p[pnt][i]->right){
-                    p[pnt^1].push_back(p[pnt][i]->right);
-                }
-            }
-            pnt ^= 1;
+            f.swap(g);
         }
         return res;
     }
