af2f6098e8de8f84572ac622698862ffe07524aa
lccycc <lccycc123@gmail.com>
2014-06-17 11:39:50 +0800

    Update Longest Valid Parentheses.cpp

--- Longest Valid Parentheses.cpp
+++ Longest Valid Parentheses.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
+    /*  stack
+        stack is built from the xth character of s, initally to -1
+        for each char s[i], if it's a left par (,  push i into stack;
+        if ),
+            1 stack has only one element, that means, ( is used up. 
+                rebuild stack by updating the last one of stack with i
+            2 there are more than one element in the stack, 
+                pop one, and get valid para string from stack.top()+1 -> i;
+                validate the answer with i - stack.top();
+    */
+            
     int longestValidParentheses(string s) {
-        stack<int> que;
-        que.push(0);
-        int res = 0;
-        vector<int> f;
-        f.push_back(0);
+        stack<int> p;
+        p.push(-1);
+        int ans = 0;
         for (int i = 0; i<s.size(); i++){
-            if (s[i] == '('){
-                 que.push(i+1);
-                 f.push_back(0);
-            }
+            if (s[i] == '(') p.push(i);
+            else if (p.size() == 1) p.top() = i;
             else{
-                if (que.size()>1){
-                    que.pop();
-                    f.push_back(f[que.top()] + i+1-que.top());
-                    res = max(res, f.back());
-                }else{
-                    que.pop();
-                    que.push(i+1);
-                    f.push_back(0);
-                }
+                p.pop();
+                ans = max(ans, i-p.top());
             }
         }
-        return res;
+        return ans;
     }
 };
