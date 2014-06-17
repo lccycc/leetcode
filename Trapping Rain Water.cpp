25f230378940a3b3297520dc88afb695b4f609a5
lccycc <lccycc123@gmail.com>
2014-06-17 16:31:51 +0800

    Update Trapping Rain Water.cpp

--- Trapping Rain Water.cpp
+++ Trapping Rain Water.cpp
@@ -1,30 +1,19 @@
 class Solution {
 public:
     int trap(int A[], int n) {
-        int h = 0;
-        int tot = 0;
-        int deta = 0;
-        int pnti = 0;
-        for (int i = 0; i<n; i++){
-            if (A[i] >= h){
-                tot += deta;
-                deta = 0;
-                h = A[i];
-                pnti = i;
-            }else{
-                deta +=h - A[i];
-            }
+        if (n == 0) return 0;
+        int maxh = 0;
+        for (int i = 0; i<n; i++) if (A[i] > A[maxh]) maxh = i;
+        int ans = 0, maxbar = 0;
+        for (int i = 0; i<maxh; i++){
+            maxbar = max(maxbar, A[i]);
+            ans += maxbar - A[i];
         }
-        h = deta = 0;
-        for (int i = n-1; i>=pnti; i--){
-            if (A[i] >= h){
-                tot += deta;
-                deta = 0;
-                h = A[i];
-            }else{
-                deta +=h - A[i];
-            }
+        maxbar = 0;
+        for (int i = n-1; i>maxh; i--){
+            maxbar = max(maxbar, A[i]);
+            ans += maxbar - A[i];
         }
-        return tot;
+        return ans;
     }
 };
