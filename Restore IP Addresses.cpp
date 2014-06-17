cff23722228104dcc9c50557ef3f2dfd8b78a944
lccycc <lccycc123@gmail.com>
2014-06-17 16:01:17 +0800

    Update Restore IP Addresses.cpp

--- Restore IP Addresses.cpp
+++ Restore IP Addresses.cpp
@@ -1,30 +1,34 @@
+/*
+    should use up all char in s
+    should be exactly 4 number
+    one number should be 0..255
+    a number can be a single '0'. if not, should not have leading '0'
+*/
 class Solution {
 public:
     vector<string> res;
-    int c[4];
-    void dfs(string &s, int k, int len, int deta, int cp){
-        if (cp >=4 ){
-            if (k < s.size() || len != 0) return ;
-            char tmp[20];
-            sprintf(tmp, "%d.%d.%d.%d", c[0], c[1], c[2], c[3]);
-            res.push_back(string(tmp));
+    void dfs(vector<string> &ips, int k, string &s, int pnts){
+        if (pnts == s.size() && k == 4){
+            string tmp = ips[0] + "." + ips[1] + "." + ips[2] + "." + ips[3];
+            res.push_back(tmp);
             return ;
         }
-        if (k >= s.length()) return ;
-
-        deta = deta * 10 + s[k]-'0';
-        if (deta >= 256) return ;
-
-        c[cp] = deta;
-        dfs(s, k+1, 0, 0, cp+1);
-
-        if (len == 0 && s[k] == '0') return ;
-        dfs(s, k+1, len+1, deta, cp);
-
+        if (pnts == s.size() || k>=4) return ;
+        if ((4-k)*3+pnts < s.size()) return;
+        if ((4-k) + pnts > s.size()) return ;
+        for (int i = 1; i<=3 && i+pnts<=s.size(); i++){
+            if (i>1 && s[pnts] == '0') break;
+            ips[k] = s.substr(pnts, i);
+            int num = atoi(ips[k].c_str());
+            if (num > 255) break;
+            dfs(ips, k+1, s, pnts+i);
+        }
     }
     vector<string> restoreIpAddresses(string s) {
         res.clear();
-        dfs(s, 0, 0, 0, 0);
+        vector<string> ips;
+        ips.resize(4);
+        dfs(ips, 0, s, 0);
         return res;
     }
 };
