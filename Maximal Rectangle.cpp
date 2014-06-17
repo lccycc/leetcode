5a2955b0aedcf46e379b27c524f28778cf41704e
lccycc <lccycc123@gmail.com>
2014-06-17 11:47:27 +0800

    Update Maximal Rectangle.cpp

--- Maximal Rectangle.cpp
+++ Maximal Rectangle.cpp
@@ -1,46 +1,31 @@
 class Solution {
 public:
     int maximalRectangle(vector<vector<char> > &matrix) {
-        int res = 0;
-        vector<int> l, r, h;
+        vector<int> p, l, r;
         int n = matrix.size();
         if (n == 0) return 0;
         int m = matrix[0].size();
         if (m == 0) return 0;
-        for (int i = 0; i<m; i++){
-            h.push_back(0);
-        }
-        l = r = h;
+        p.resize(m);
+        l.resize(m);
+        r.resize(m);
+        int ans = 0;
         for (int i = 0; i<n; i++){
+            for (int j = 0; j<m; j++) if (matrix[i][j] == '1') p[j]++; else p[j] = 0;
             for (int j = 0; j<m; j++){
-                if (matrix[i][j] == '0'){
-                    h[j] = 0;
-                }else{
-                    h[j]++;
+                l[j] = j-1;
+                while (l[j] >= 0 && p[l[j]] >= p[j]){
+                    l[j] = l[l[j]];
                 }
-            }
-            for (int j = 0; j<m; j++){
-                int p = j-1;
-                while (p>=0){
-                    if (h[p] < h[j]) break;
-                    p = l[p];
-                }
-                l[j] = p;
             }
             for (int j = m-1; j>=0; j--){
-                int p = j+1;
-                while (p<m){
-                    if (h[p] < h[j]) break;
-                    p = r[p];
+                r[j] = j+1;
+                while (r[j] <m && p[r[j]] >= p[j]){
+                    r[j] = r[r[j]];
                 }
-                r[j] = p;
             }
-            for (int j = 0; j<m; j++){
-                res = max(res, h[j] * (r[j]-l[j]-1));
-            }
+            for (int j = 0; j<m; j++) ans = max(ans, p[j] * (r[j] - l[j] - 1));
         }
-        return res;
+        return ans;
     }
 };
-
-
