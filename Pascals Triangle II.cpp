53259ec1e0fcad5a42f3f1f32b60b8e976af52ac
lccycc <lccycc123@gmail.com>
2014-06-17 15:23:11 +0800

    Update Pascals Triangle II.cpp

--- Pascals Triangle II.cpp
+++ Pascals Triangle II.cpp
@@ -1,14 +1,12 @@
+/* the rowIndex start at 0 */
 class Solution {
 public:
     vector<int> getRow(int rowIndex) {
-        vector<int> tp, rp;
+        vector<int> a;
         for (int i = 0; i<=rowIndex; i++){
-            tp.push_back(1);
-            for (int j = 1; j<i; j++){
-                tp[j] = rp[j-1]+rp[j];
-            }
-            rp = tp;
+            for (int j = a.size()-1; j>0; j--) a[j] += a[j-1];
+            a.push_back(1);
         }
-        return rp;
+        return a;
     }
 };
