08ece66774c0e792b4ba94b8c6d7feb2f4692bb2
lccycc <lccycc123@gmail.com>
2014-06-17 11:00:28 +0800

    Update Distinct Subsequences.cpp

--- Distinct Subsequences.cpp
+++ Distinct Subsequences.cpp
@@ -3,9 +3,7 @@ public:
     int numDistinct(string S, string T) {
         vector<int> l;
         int n = S.size(), m = T.size();
-        for (int i = 0; i<=m; i++){
-            l.push_back(0);
-        }
+        l.resize(m+1);
         l[0] = 1;
         for (int i = 1; i<=n; i++){
             for (int j = m; j>0; j--){
