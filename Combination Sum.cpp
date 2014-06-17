db9db097904fdceb5581694e5c2e1af5cf17f72e
lccycc <lccycc123@gmail.com>
2014-06-17 10:39:14 +0800

    Update Combination Sum.cpp

--- Combination Sum.cpp
+++ Combination Sum.cpp
@@ -1,38 +1,30 @@
 class Solution {
 public:
     vector<vector<int> > res;
-    vector<int> s;
-    vector<int> ck;
-    void dfs(int k, int target){
-        if (target == 0){
-            vector<int> tmp;
-            for (int i = 0; i<s.size(); i++){
-                for (int j = 0; j<ck[i]; j++){
-                    tmp.push_back(s[i]);
-                }
-            }
+    vector<int> tmp;
+    void recur(vector<int> &candidates, int k, int sum){
+        /* we want to get to sum using candidates[k..n-1] */
+        if (sum == 0) {
+            /* output result */
             res.push_back(tmp);
             return ;
         }
-        if (k >= s.size()) return ;
-        if (target < ck[k]) return ;
-        for (ck[k] = 0; target >=0; ck[k]++){
-            dfs(k+1, target);
-            target -= s[k];
+        if (sum < 0) return ;
+        if (k>=candidates.size()){
+            return ;
         }
-        ck[k] = 0;
+        tmp.push_back(candidates[k]);
+        recur(candidates, k, sum - tmp.back());
+        tmp.erase(tmp.end()-1);
+        recur(candidates, k+1, sum);
+        
     }
     vector<vector<int> > combinationSum(vector<int> &candidates, int target) {
-        s = candidates;
-        sort(s.begin(), s.end());
-        s.erase(unique(s.begin(), s.end()), s.end());
-        int n = s.size();
-        ck.clear();
-        for (int i = 0; i<n; i++){
-            ck.push_back(0);
-        }
         res.clear();
-        dfs(0, target);
+        tmp.clear();
+        sort(candidates.begin(), candidates.end());
+        candidates.erase(unique(candidates.begin(), candidates.end()), candidates.end());
+        recur(candidates, 0, target);
         return res;
     }
 };
