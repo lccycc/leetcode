8e823f560fac8df403886437b1f2ffe244e60a30
lccycc <lccycc123@gmail.com>
2014-06-17 11:28:27 +0800

    Update Linked List Cycle II.cpp

--- Linked List Cycle II.cpp
+++ Linked List Cycle II.cpp
@@ -1,3 +1,10 @@
+/*
+    consider the sub index of p as x when p meets q. head has subindex of 0
+    assume the length of loop ls L
+    then, q has a sub index of 2x, we got that L | 2x-x  -> L | x
+    so p differs with head for several whole loop. they will meet.
+    and the meet point must be the start of loop
+*/
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
