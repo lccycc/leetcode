b6b402e3c7056ef503e8e6092818f0a039735b9e
lccycc <lccycc123@gmail.com>
2014-06-17 10:54:18 +0800

    Update Container With Most Water.cpp

--- Container With Most Water.cpp
+++ Container With Most Water.cpp
@@ -1,3 +1,10 @@
+/*
+    assume that 0..i-1 and j+1..n-1 have all find their best match
+    assume height[i] < height[j]
+    if i match some k, i<k<j
+        answer = (k-i)*min(height[i], height[k]) <= (k-i)*height[i] < (j-i)*height[i];
+    so the best match of i is j.  for k <i or k>j, they have find their best match
+*/
 class Solution {
 public:
     int maxArea(vector<int> &height) {
