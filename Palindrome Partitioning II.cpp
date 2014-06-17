01dfe9648fcd2c4a236e8f38bd8413dddb214436
lccycc <lccycc123@gmail.com>
2014-06-17 15:07:43 +0800

    Update Palindrome Partitioning II.cpp

--- Palindrome Partitioning II.cpp
+++ Palindrome Partitioning II.cpp
@@ -1,39 +1,25 @@
 class Solution {
 public:
-    bool ispara(string &s, int l, int r){
-        for (;l<r; l++, r--){
-            if (s[l] != s[r]) return false;
-        }
-        return true;
-    }
     int minCut(string s) {
         if (s.size() == 0) return 0;
-        vector<int> p;
         vector<vector<int> > f;
         f.resize(s.size());
         for (int i = 0; i<s.size(); i++){
-            int j = 0;
-            while (i-j>=0 && i+j<s.size() && s[i-j] == s[i+j]){
-                f[i+j].push_back(i-j);
-                j++;
+            for (int len = 0; i-len>=0 && i+len<s.size() && s[i-len] == s[i+len]; len++){
+                f[i-len].push_back(i+len+1);
             }
-            j = 0;
-            while (i-j>=0 && i+j+1<s.size() && s[i-j] == s[i+j+1]){
-                f[i+j+1].push_back(i-j);
-                j++;
+            for (int len = 0; i-len>=0 && i+len+1<s.size() && s[i-len] == s[i+len+1]; len++){
+                f[i-len].push_back(i+len+2);
             }
         }
-        p.push_back(0);
+        vector<int> a;
+        a.resize(s.size()+1);
+        for (int i = 0; i<=s.size(); i++) a[i] = i;
         for (int i = 0; i<s.size(); i++){
-            int x = s.size();
-            for (int kj = 0; kj<f[i].size(); kj++){
-                int j = f[i][kj];
-                x = min(x, p[j]+1);
+            for (int nex : f[i]){
+                a[nex] = min(a[nex], a[i]+1);
             }
-            p.push_back(x);
         }
-        return p.back()-1;
+        return a[s.size()]-1;
     }
 };
-
-
