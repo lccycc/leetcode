7dba7334a4e95760f3fd06c02f102ac17da73c4d
lccycc <lccycc123@gmail.com>
2014-06-17 16:32:41 +0800

    Update Triangle.cpp

--- Triangle.cpp
+++ Triangle.cpp
@@ -1,26 +1,17 @@
 class Solution {
 public:
     int minimumTotal(vector<vector<int> > &triangle) {
-        int n = triangle.size();
-        vector<int> a[2];
-        for (int i = 0; i<n; i++){
-            a[0].push_back(0);
-            a[1].push_back(0);
-        }
-        int t = 0;
-        a[t][0] = triangle[0][0];
-        for (int i = 1; i<n; i++){
-            a[t^1][0] = a[t][0] + triangle[i][0];
-            for (int j = 1; j<i; j++){
-                a[t^1][j] = min(a[t][j] , a[t][j-1]) + triangle[i][j];
+        for (int i = 1; i<triangle.size(); i++){
+            triangle[i][0] += triangle[i-1][0];
+            triangle[i].back() += triangle[i-1].back();
+            for (int j = 1; j+1<triangle[i].size(); j++){
+                triangle[i][j] += min(triangle[i-1][j], triangle[i-1][j-1]);
             }
-            a[t^1][i] = a[t][i-1] + triangle[i][i];
-            t^=1;
         }
-        int res = INT_MAX;
-        for (int i = 0; i<n; i++){
-            res = min(res, a[t][i]);
+        int ans = triangle.back()[0];
+        for (int x : triangle.back()){
+            ans = min(ans, x);
         }
-        return res;
+        return ans;
     }
 };
