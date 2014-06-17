a0b85904dd51e781ae64b39896c05b46c1836636
lccycc <lccycc123@gmail.com>
2014-06-17 10:27:24 +0800

    Update Binary Tree Level Order Traversal II.cpp

--- Binary Tree Level Order Traversal II.cpp
+++ Binary Tree Level Order Traversal II.cpp
@@ -1,30 +1,23 @@
+/*
+    clear g  at the beginning of loop
+    swap f, g at the end of loop
+*/
 class Solution {
 public:
-    vector<vector<int> > levelOrderBottom(TreeNode *root) {
-        vector<TreeNode *> p[2];
-        int pnt = 0;
+    vector<vector<int> > levelOrder(TreeNode *root) {
+        vector<TreeNode *> f, g;
         vector<vector<int> > res;
-        if (root == NULL) return res;
-        p[0].push_back(root);
-        while (p[pnt].size()){
+        if (!root) return res;
+        f.push_back(root);
+        while (f.size()){
             res.push_back(vector<int>());
-            for (int i = 0; i<p[pnt].size(); i++){
-                res.back().push_back(p[pnt][i]->val);
+            g.clear();
+            for (auto x : f){
+                res.back().push_back(x->val);
+                if (x->left) g.push_back(x->left);
+                if (x->right) g.push_back(x->right);
             }
-
-            p[pnt^1].clear();
-            for (int i = 0; i<p[pnt].size(); i++){
-                if (p[pnt][i]->left){
-                    p[pnt^1].push_back(p[pnt][i]->left);
-                }
-                if (p[pnt][i]->right){
-                    p[pnt^1].push_back(p[pnt][i]->right);
-                }
-            }
-            pnt^=1;
-        }
-        for (int l = 0, r = res.size()-1; l<r; l++, r--){
-            swap(res[l], res[r]);
+            f.swap(g);
         }
         return res;
     }
