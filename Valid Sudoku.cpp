c2dd1f07d2631d8c847a1cb7a8b3140320876e10
lccycc <lccycc123@gmail.com>
2014-06-17 16:37:28 +0800

    Update Valid Sudoku.cpp

--- Valid Sudoku.cpp
+++ Valid Sudoku.cpp
@@ -1,22 +1,14 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char> > &board) {
-        bool row[9][9], col[9][9], sqr[3][3][9];
-        memset(row, 0, sizeof(row));
-        memset(col, 0, sizeof(col));
-        memset(sqr, 0, sizeof(sqr));
-        for (int i = 0; i<9; i++){
-            for (int j = 0; j<9; j++){
-                if (board[i][j]!='.'){
-                    if (row[i][board[i][j]-'1']) return false;
-                    row[i][board[i][j]-'1'] = true;
-                    int x = i/3, y = j/3;
-                    if (sqr[x][y][board[i][j]-'1']) return false;
-                    sqr[x][y][board[i][j]-'1'] = true;
-                    if (col[j][board[i][j]-'1']) return false;
-                    col[j][board[i][j]-'1'] = true;
-                }
-            }
+        bool row[9][9]={}, col[9][9]={}, sqr[3][3][9]={};
+        for (int i = 0; i<9; i++) for (int j = 0; j<9; j++){
+            char c = board[i][j];
+            if (c == '.') continue;
+            if (c<'1' || c>'9') return false;
+            c -= '1';
+            if (row[i][c] || col[j][c] || sqr[i/3][j/3][c]) return false;
+            row[i][c] = col[j][c] = sqr[i/3][j/3][c] = true;
         }
         return true;
     }
