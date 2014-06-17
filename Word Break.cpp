590267364c82257203ed8efd6a75cd8b8706c5b7
lccycc <lccycc123@gmail.com>
2014-06-17 16:41:14 +0800

    Update Word Break.cpp

--- Word Break.cpp
+++ Word Break.cpp
@@ -1,23 +1,15 @@
 class Solution {
 public:
     bool wordBreak(string s, unordered_set<string> &dict) {
-        vector<bool> visit;
-        for (int i = 0; i<=s.size(); i++){
-            visit.push_back(false);
+        int n = s.size();
+        vector<bool> dp;
+        dp.resize(n+1);
+        dp[0] = true;
+        for (int i = 0; i<n; i++) if (dp[i]){
+            for (int x = i+1; x<=n; x++) 
+                if (dict.find(s.substr(i, x-i)) != dict.end())
+                    dp[x] = true;
         }
-        visit[0] = true;
-        for (int i = 0; i<s.size(); i++){
-            if (visit[i]){
-                for (unordered_set<string>::iterator it = dict.begin();
-                        it != dict.end(); it++){
-                    int len = (*it).size();
-                    if (i + len > s.size()) continue;
-                    if (s.substr(i, len).compare(*it) == 0){
-                        visit[i+len] = true;
-                    }
-                }
-            }
-        }
-        return visit[s.size()];
+        return dp[n];
     }
 };
