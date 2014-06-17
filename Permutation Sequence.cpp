e3b73196bd9c369d1314c2dcf99d37bb80304dfd
lccycc <lccycc123@gmail.com>
2014-06-17 15:30:36 +0800

    Update Permutation Sequence.cpp

--- Permutation Sequence.cpp
+++ Permutation Sequence.cpp
@@ -1,22 +1,19 @@
+class Solution {
 public:
     string getPermutation(int n, int k) {
-        vector<int> step;
-        step.push_back(1);
-        for (int i = 1; i<n; i++) step.push_back(step[i-1]*i);
-        string res;
         k--;
-        bool visit[11] = {};
-        for (int i = 0; i<n; i++){
-            int base = k/step[n-i-1];
-            int j;
-            for (j = 0; j<n; j++) if (!visit[j]){
-                if (base == 0) break;
-                base--;
-            }
-            visit[j] = true;
-            k %= step[n-i-1];
-            res = res + char('1'+j);
+        int bas = 1;
+        for (int i = 1; i<=n; i++) bas = bas*i;
+        string s(n, ' ');
+        vector<char> ch;
+        for (int i = 0; i<n; i++) ch.push_back('1'+i);
+        for (int i = n; i>=1; i--){
+            bas = bas/i;
+            int let = k / bas;
+            k = k%bas;
+            s[n-i] = ch[let];
+            ch.erase(ch.begin()+let);
         }
-        return res;
+        return s;
     }
 };
