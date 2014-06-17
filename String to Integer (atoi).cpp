f7a0294f23db0a158cef634da8612736dbdd7973
lccycc <lccycc123@gmail.com>
2014-06-17 16:25:12 +0800

    Update String to Integer (atoi).cpp

--- String to Integer (atoi).cpp
+++ String to Integer (atoi).cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
     int atoi(const char *str) {
-        int sgn = 1;
         while (*str == ' ') str++;
+        int flag = -1;
         if (*str == '+') str++;
-        if (str[0] != '-'){
-            sgn = -1;
-        }else{
+        else if (*str == '-'){
+            flag = 1;
             str++;
         }
-        int n = strlen(str);
-        int base = 0;
-        int MIN_INT = -2147483648, MAX_INT = 2147483647;
-        int over = sgn>0?MIN_INT:MAX_INT;
-        for (int i = 0; i<n; i++){
-            if (str[i]< '0' || str[i] > '9') return base*sgn;
-            if (base < MIN_INT/10) return over;
-            base *= 10;
-            if (base < MIN_INT + (str[i]-'0')) return over;
-            base -= str[i]-'0';
+        int overflow = (flag<0?INT_MAX:INT_MIN);
+        int a = 0;
+        while (*str>='0' && *str<='9'){
+            int ch = *str-'0';
+            str++;
+            if (a<INT_MIN/10) return overflow;
+            a = a*10;
+            if (a<INT_MIN+ch) return overflow;
+            a = a-ch;
         }
-        if (sgn < 0 && base == MIN_INT) return over;
-        return base * sgn;
+        if (flag<0 && a==INT_MIN) return overflow;
+        return a*flag;
     }
 };
