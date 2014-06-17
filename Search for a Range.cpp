b051c037622c5b117449302f24b344ef87db4103
lccycc <lccycc123@gmail.com>
2014-06-17 16:13:42 +0800

    Update Search for a Range.cpp

--- Search for a Range.cpp
+++ Search for a Range.cpp
@@ -1,42 +1,34 @@
 class Solution {
 public:
-    int lowerbound(int A[], int l, int r, int target){
-        //find minimal l that A[l] >= target
+    int upbound(int A[], int n, int target){
+        /* minimal i that A[i] > target */
+        int l = 0, r = n;
+        if (target < A[0]) return 0;
         while (l+1<r){
             int mid = (l+r)/2;
-            if (A[mid] >= target){
-                r = mid;
-            }else{
-                l = mid;
-            }
+            if (A[mid] <= target) l = mid;
+            else r = mid;
         }
-        if (A[l] < target) return l+1;
-        return l;
+        return r;
     }
-    int upperbound(int A[], int l, int r, int target){
-        //find maximal l that A[l] <= target
+    int lowbound(int A[], int n, int target){
+        /* minimal i that A[i] >= target */
+        if (A[0] >= target) return 0;
+        int l = 0, r = n;
         while (l+1<r){
             int mid = (l+r)/2;
-            if (A[mid] <= target){
-                l = mid;
-            }else{
-                r = mid;
-            }
+            if (A[mid] < target) l = mid;
+            else r = mid;
         }
-        if (A[l] > target) return l-1;
-        return l;
-}
+        return r;
+    }
     vector<int> searchRange(int A[], int n, int target) {
-        vector<int> res;
-        res.push_back(-1);
-        res.push_back(-1);
-        int l = lowerbound(A, 0, n, target);
-        if (l >= n || A[l] != target){
-            return res;
-        }
-        int r = upperbound(A, 0, n, target);
+        vector<int> res({-1, -1});
+        int l = lowbound(A, n, target);
+        if (l>=n || A[l] != target) return res;
+        int r = upbound(A, n, target);
         res[0] = l;
-        res[1] = r;
+        res[1] = r-1;
         return res;
-    }   
+    }
 };
